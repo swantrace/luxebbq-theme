8efd73cdbccbc5451ed84afa1ae824f180a645a5
Qixuan Hong <frederickhong.5@gmail.com>
2021-07-27 14:53:21 -0700

    make a way to add custom html to top images area

--- src/components/productTypeCollection/SidebarTopImages.js
+++ src/components/productTypeCollection/SidebarTopImages.js
@@ -1,17 +1,30 @@
 import { html, virtual } from '@apollo-elements/haunted';
+import { unsafeHTML } from 'lit-html/directives/unsafe-html';
 
-const SidebarTopImages = virtual(
-  ({ collectionImages }) => html`<div class="collection-images-wrapper">
-    ${collectionImages.map(
-      (image) => html`
-      <div class="collection-image-wrapper py-2" style="width: 100%; min-height: 100px;">
-        <a href=${image.imageTarget}
-          ><img class="w-100" src=${image.imageUrl} alt=${image.imageText}
-        /></a>
-      </div></div>
-    `
-    )}
-  </div>`
-);
+const SidebarTopImages = virtual(({ collectionImages }) => {
+  const topImagesPlaceHolder = document.querySelector(
+    '#top-images-placeholder'
+  );
+  console.log(topImagesPlaceHolder);
+  return html`<div class="collection-images-wrapper">
+    ${topImagesPlaceHolder.innerHTML
+      ? html`${unsafeHTML(topImagesPlaceHolder.innerHTML)}`
+      : html`${collectionImages.map(
+          (image) => html`
+            <div
+              class="collection-image-wrapper py-2"
+              style="width: 100%; min-height: 100px;"
+            >
+              <a href=${image.imageTarget}
+                ><img
+                  class="w-100"
+                  src=${image.imageUrl}
+                  alt=${image.imageText}
+              /></a>
+            </div>
+          `
+        )}`}
+  </div>`;
+});
 
 export default SidebarTopImages;
